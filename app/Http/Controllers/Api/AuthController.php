9a1c5c321ec9f98f642efbd20c88bf74452ba3ec
tieppt07 <tieppt07@gmail.com>
2016-09-24 08:12:29 +0700

    Routes: Api, admin, theme - Api AuthController

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Index login controller
+     *
+     * When user success login will retrive callback as api_token
+     */
+    public function login(Request $request)
+    {
+        $hasher = app()->make('hash');
+
+        $username = $request->get('username');
+        $password = $request->get('password');
+
+        $user = User::where('username', $username)->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Your username or password incorrect!'
+            ]);
+        }
+
+        if ( !$hasher->check($password, $user->password) ) {
+            return response()->json([
+                'message' => 'Your username or password incorrect!'
+            ]);
+        }
+
+        $api_token = sha1(time());
+        $create_token = User::where('id', $user->id)->update(['api_token' => $api_token]);
+
+        return response([
+            'success' => true,
+            'message' => 'Logged in!',
+            'api_token' => $api_token,
+            'authUser' => \Auth::user(),
+        ]);
+    }
+
+    /**
+     * Register new user
+     *
+     * @param $request Request
+     */
+    public function register(Request $request)
+    {
+        $hasher = app()->make('hash');
+
+        $request->merge(array_map('trim', $request->except(['password', 'password_confirmation'])));
+        $this->validate($request, [
+            'name' => 'required|unique:users',
+            'username' => 'required|alpha_dash|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        $data = $request->all();
+        $data['password'] = $hasher->make($request->get('password'));
+
+        $register = User::create($data);
+
+        if ($register) {
+            return response([
+                'success' => true,
+                'message' => 'Success register!',
+            ]);
+        }else{
+            return response([
+                'success' => false,
+                'message' => 'Failed to register!',
+            ]);
+        }
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,17 +11,64 @@
 |
 */
 
-$app->get('/', function () use ($app) {
-    return $app->version();
-});
-
-$app->post('login', 'Auth\AuthController@login');
-$app->post('register', 'Auth\AuthController@register');
-
 $app->group(['prefix' => 'api', 'namespace' => 'App\Http\Controllers\Api'], function () use ($app) {
+    $app->post('login', 'AuthController@login');
+    $app->post('register', 'AuthController@register');
+
     $app->get('users', 'UsersController@index');
     $app->get('user/{user}', 'UsersController@show');
     $app->post('user', 'UsersController@store');
     $app->patch('user/{user}', 'UsersController@update');
     $app->delete('user/{user}', 'UsersController@destroy');
+});
+
+$app->group(['namespace' => 'App\Http\Controllers\Auth'], function () use ($app) {
+    $app->get('login', 'AuthController@showLogin');
+    $app->post('login', 'AuthController@login');
+    $app->get('register', 'AuthController@showresgister');
+    $app->post('register', 'AuthController@register');
+});
+
+$app->group(['prefix' => 'admin', 'namespace' => 'App\Http\Controllers\Admin'], function () use ($app) {
+    $app->get('/', 'HomeController@index');
+    $app->get('profile', 'ProfileController@index');
+    $app->patch('profile/info', 'ProfileController@info');
+    $app->patch('profile/password', 'ProfileController@password');
+
+    $app->get('users', 'UsersController@index');
+    $app->get('user/create', 'UsersController@create');
+    $app->post('user', 'UsersController@store');
+    $app->get('user/{user}', 'UsersController@show');
+    $app->get('user/{user}/edit', 'UsersController@edit');
+    $app->patch('user/{user}', 'UsersController@update');
+    $app->delete('user/{user}', 'UsersController@destroy');
+
+    $app->get('categories', 'CategoriesController@index');
+    $app->get('category/create', 'CategoriesController@create');
+    $app->post('category', 'CategoriesController@store');
+    $app->get('category/{category}', 'CategoriesController@show');
+    $app->get('category/{category}/edit', 'CategoriesController@edit');
+    $app->patch('category/{category}', 'CategoriesController@update');
+    $app->delete('category/{category}', 'CategoriesController@destroy');
+
+    $app->get('tags', 'TagsController@index');
+    $app->get('tag/create', 'TagsController@create');
+    $app->post('tag', 'TagsController@store');
+    $app->get('tag/{tag}', 'TagsController@show');
+    $app->get('tag/{tag}/edit', 'TagsController@edit');
+    $app->patch('tag/{tag}', 'TagsController@update');
+    $app->delete('tag/{tag}', 'TagsController@destroy');
+
+    $app->get('posts', 'PostsController@index');
+    $app->get('post/create', 'PostsController@create');
+    $app->post('post', 'PostsController@store');
+    $app->get('post/{post}', 'PostsController@show');
+    $app->get('post/{post}/edit', 'PostsController@edit');
+    $app->patch('post/{post}', 'PostsController@update');
+    $app->delete('post/{post}', 'PostsController@destroy');
+});
+
+$app->group(['namespace' => 'App\Http\Controllers\Theme'], function () use ($app) {
+    $app->get('/', 'HomeController@index');
+    $app->get('profile', 'ProfileController@index');
 });
